87379494f9644eabda541afae9c3d54093559395
DaMoo <git@damoo.zone>
2018-11-30 11:24:01 -0500

    Complete sum-of-multiples exercise.
    I really enjoyed using LINQ here. It's a different way of functional
    programming that I'm used to with other language's `map`,`reduce`,
    'filter`, etc.

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return multiples.Select(i => GetMultiplesUpToLimit(i, max))
+                .SelectMany(x => x)
+                .Distinct()
+                .Sum();
+    }
+
+    private static IEnumerable<int> GetMultiplesUpToLimit(int multiple, int max)
+    {
+        if (multiple == 0)
+        {
+            return new[] { 0 };
+        }
+        else
+        {
+            return Enumerable.Range(1, max / multiple)
+                    .Select(i => i * multiple)
+                    .Where(i => i != max);
+        }
+    }
+}
