412f70c5c247c5e9efe550ea4702942461808629
DaMoo <git@damoo.zone>
2018-12-03 21:24:47 -0500

    Complete raindrops exercise

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,21 @@
+using System;
+
+public static class Raindrops {
+  public static string Convert (int number) {
+    string retVal = "";
+
+    if (number % 3 == 0) {
+      retVal += ("Pling");
+    }
+    if (number % 5 == 0) {
+      retVal += ("Plang");
+    }
+    if (number % 7 == 0) {
+      retVal += ("Plong");
+    }
+    if (retVal.Length == 0) {
+      return number.ToString ();
+    }
+    return retVal;
+  }
+}
