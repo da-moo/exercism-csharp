165ce55f69ae9b3ed484e2beec58f817ae93fe38
DaMoo <git@damoo.zone>
2018-12-05 18:26:32 -0500

    Implement armstrong-numbers exercise

---
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class ArmstrongNumbers {
+  public static bool IsArmstrongNumber (int number) {
+    var digits = number.ToString ().Select (i => int.Parse (i.ToString ()));
+    int numOfDigits = digits.Count ();
+    int armstrong = digits.Aggregate (0, (currentTotal, nextValue) =>
+      currentTotal + (int) Math.Pow (nextValue, numOfDigits));
+    return number == armstrong;
+  }
+}
