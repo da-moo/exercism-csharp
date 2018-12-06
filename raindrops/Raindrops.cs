0a21dc125419ca5020252d1e3ec8ec2a8780b7f0
DaMoo <git@damoo.zone>
2018-12-06 12:16:47 -0500

    Refactor based on recommendations

--- raindrops/Raindrops.cs
+++ raindrops/Raindrops.cs
@@ -2,19 +2,19 @@ using System;
 
 public static class Raindrops {
   public static string Convert (int number) {
-    string retVal = "";
+    string retVal = string.Empty;
 
     if (number % 3 == 0) {
-      retVal += ("Pling");
+      retVal += "Pling";
     }
     if (number % 5 == 0) {
-      retVal += ("Plang");
+      retVal += "Plang";
     }
     if (number % 7 == 0) {
-      retVal += ("Plong");
+      retVal += "Plong";
     }
     if (retVal.Length == 0) {
-      return number.ToString ();
+      return number.ToString();
     }
     return retVal;
   }
