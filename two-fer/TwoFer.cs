0fda5ca3c25b6cae57ba7b4820ce696bda57198d
DaMoo <git@damoo.zone>
2018-12-07 15:48:50 -0500

    Implement two-fer exercise

---
+++ two-fer/TwoFer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoFer
+{
+    public static class TwoFer
+    {
+        public static string Name(string input = null)
+        {
+            return $"One for {input ?? "you"}, one for me.";
+        }
+    }
+}
