2f9b36db16be9e94e26ccf552927f6b96d7424a1
DaMoo <git@damoo.zone>
2018-12-12 16:45:51 -0500

    Implement recursive solution for collatz-conjecture exercise

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number, int steps = 0)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentException("Only numbers greater than zero are allowed");
+        }
+
+        if (number == 1) return steps;
+        if (number % 2 == 0) return Steps(number / 2, ++steps);
+        return Steps(3 * number + 1, ++steps);
+
+    }
+}
