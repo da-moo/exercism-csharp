b7299c462aea2c9d6fdce00473aaa7fd76ff727f
DaMoo <git@damoo.zone>
2018-12-14 13:42:23 -0500

    Complete grains exercise

---
+++ grains/Grains.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+public static class Grains
+{
+    private const int NumSquares = 64;
+
+    public static ulong Square(int n) => n <= 0 || n > NumSquares
+            ? throw new ArgumentOutOfRangeException(nameof(n), "Must be between 1 and 64 (inclusive)")
+            : (ulong)Math.Pow(2, n - 1);
+
+    public static ulong Total() => (ulong)(BigInteger.Pow(2, NumSquares) - 1);
+}
