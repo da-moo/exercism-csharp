df4ee575505870adaea5135de3c921888d529dbb
DaMoo <git@damoo.zone>
2018-12-17 17:18:50 -0500

    Port pangram typescript solution to c#

---
+++ pangram/Pangram.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    private static readonly Regex AlphaUniqueCountPattern = new Regex(@"([a-z])(?!.*\1)", RegexOptions.Compiled);
+    public static bool IsPangram(string input) => AlphaUniqueCountPattern.Matches(input.ToLower()).Count == 26;
+}
