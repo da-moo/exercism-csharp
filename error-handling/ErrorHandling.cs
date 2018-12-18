732bf8e0f3bcd416dd1ac54242c10db8dd07aaf5
DaMoo <git@damoo.zone>
2018-12-18 18:07:45 -0500

    Implement error-handling exercise

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception("Example exception");
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return int.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception("Disposable object should be disposed");
+        }
+    }
+}
