21bf4848c953f538c2edda3dea56ec0a1226c67b
DaMoo <git@damoo.zone>
2018-12-24 10:27:56 -0500

    Implement strain exercise

---
+++ strain/Strain.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Strain
+{
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        return collection.Where(predicate);
+    }
+
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+    {
+        foreach (var item in collection)
+        {
+            if (!predicate(item)) yield return item;
+        }
+    }
+}
