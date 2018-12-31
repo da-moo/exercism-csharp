2462b133b738d00798fb3cecc6218c15688c06c9
DaMoo <git@damoo.zone>
2018-12-31 18:50:28 -0500

    Finish implementing clock exercise

--- clock/Clock.cs
+++ clock/Clock.cs
@@ -1,16 +1,23 @@
 using System;
 
-public class Clock
+public class Clock : IEquatable<Clock>
 {
     public Clock(int hours, int minutes)
     {
-        Hours = (hours + (minutes / 60)) % 24;
-        Minutes = minutes % 60;
+        if (minutes < 0)
+        {
+            Hours = Mod(hours - (Math.Abs(minutes) + 60) / 60, 24);
+        }
+        else
+        {
+            Hours = Mod(hours + (minutes / 60), 24);
+        }
+        Minutes = Mod(minutes, 60);
     }
 
-    public int Hours { get; }
+    private int Hours { get; }
 
-    public int Minutes { get; }
+    private int Minutes { get; }
 
     public Clock Add(int minutesToAdd)
     {
@@ -19,8 +26,27 @@ public class Clock
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new Clock(Hours, Minutes - minutesToSubtract);
     }
 
     public override string ToString() => $"{Hours:00}:{Minutes:00}";
+
+    private static int Mod(int x, int y) {
+        return (x % y + y) % y;
+    }
+
+    public bool Equals(Clock other)
+    {
+        return Hours == other.Hours && Minutes == other.Minutes;
+    }
+
+    public static bool operator ==(Clock left, Clock right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Clock left, Clock right)
+    {
+        return !Equals(left, right);
+    }
 }
