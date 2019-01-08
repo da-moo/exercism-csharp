414bcbe612bc918827ab1db8bd70a6fd4f346444
DaMoo <git@damoo.zone>
2019-01-08 13:44:00 -0500

    Implement secret-handshake exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var sanitizedWord = word.ToLower().TakeWhile(x => x != ' ' && x != '-');
+        return sanitizedWord.Count() == sanitizedWord.Distinct().Count();
+    }
+}
