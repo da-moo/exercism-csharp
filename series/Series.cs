28288a9aac79b6dbef3d84ca7132c9ace014aab9
DaMoo <git@damoo.zone>
2019-02-08 17:21:23 -0500

    Implement series exercise

---
+++ series/Series.cs
@@ -0,0 +1,31 @@
+using System;
+
+public static class Series
+{
+    public static string[] Slices(string numbers, int sliceLength)
+    {
+        if (sliceLength > numbers.Length)
+        {
+            throw new ArgumentException(
+                $"{nameof(sliceLength)} cannot be larger than the length of ${nameof(numbers)}");
+        }
+
+        if (string.IsNullOrEmpty(numbers))
+        {
+            throw new ArgumentException($"{nameof(numbers)} cannot be empty or null");
+        }
+
+        if (sliceLength <= 0)
+        {
+            throw new ArgumentException($"{nameof(sliceLength)} cannot be less than 1");
+        }
+
+        string[] retValStrings = new string[numbers.Length - sliceLength + 1];
+        for (int i = 0; i <= numbers.Length - sliceLength; i++)
+        {
+            retValStrings[i] = numbers.Substring(i, sliceLength);
+        }
+
+        return retValStrings;
+    }
+}
