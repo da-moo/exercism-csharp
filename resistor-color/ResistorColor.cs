d922f1562d532865057db704b02fee45f9ea6d5b
DaMoo <git@damoo.zone>
2019-02-14 19:05:59 -0500

    Implement resistor-color exercise

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    private static readonly List<string> ColorsArray = new List<string>
+        {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    public static int ColorCode(string color)
+    {
+        var code = ColorsArray.IndexOf(color);
+
+        if (code == -1) throw new ArgumentException($"Invalid color: {color}");
+
+        return code;
+    }
+
+    public static string[] Colors()
+    {
+        return ColorsArray.ToArray();
+    }
+}
